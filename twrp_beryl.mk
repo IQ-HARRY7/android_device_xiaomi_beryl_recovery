a73baa99391c1f266eb68a99cb414a0b6f2ffe79
Khargosxh18 <nikhilyadav31071@gmail.com>
2026-08-04 01:42:58 +0530

    beryl: Inherit from emulated_storage.mk
    Change-Id: Iab20fd0afe4655137d3a9e24fb1762804c3b6971
    Signed-off-by: khargosxh18 <khargosxh18@gmail.com>

--- twrp_beryl.mk
+++ twrp_beryl.mk
@@ -15,9 +15,17 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from beryl device
 $(call inherit-product, device/xiaomi/beryl/device.mk)
 
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 PRODUCT_DEVICE := beryl
 PRODUCT_NAME := twrp_beryl
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 14 5G
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := twrp_$(PRODUCT_DEVICE)
+
+# Hide Reflash TWRP & FUSE passthrough
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.twrp.vendor_boot=true \
+    persist.sys.fuse.passthrough.enable=true
